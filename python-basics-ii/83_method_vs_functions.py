0fb70773b3f513a0dc8de5ba680ddfd678f681ee
CodingCCarpenter <Christineassists@aol.com>
2020-07-29 16:11:06 -0500

    demonstrate the difference between methods and functions in Python

---
+++ python-basics-ii/83_method_vs_functions.py
@@ -0,0 +1,21 @@
+# METHODS VS FUNCTIONS
+
+# METHODS - BUILT IN
+# list()
+# print()
+# max(2,3,4,5,6)
+# min(2,3,4,5,6)
+# input()
+
+# FUNCTIONS - CUSTOM
+def custom_func():
+    pass
+
+# functions are called by using parens (optionally for args) after func name
+custom_func()
+
+# methods are called by using dog notation
+print('string'.capitalize())
+
+# both allow us to 'do something', and differences are minimal. 
+# close attention should be paid to scope when using functions
