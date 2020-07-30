55bdb2f46e9dec7a9b57972b415368e5bf9c7ab5
CodingCCarpenter <Christineassists@aol.com>
2020-07-29 21:15:50 -0500

    plan and implement solution to return highest even number in given list

--- python-basics-ii/87_exercise_functions.py
+++ python-basics-ii/87_exercise_functions.py
@@ -1,11 +1,23 @@
 """ 
+problem:
 create a function called highest_even that takes a list
 as an argument, and returns the highest even number 
+
+steps I think I should take to solve:
+    1. isolate even numbers in separate list
+    2. return highest number on evens list
  
  """
 
-
 def highest_even(integer_list):
-    pass
+    evens = []
+    
+    # isolate the even numbers
+    for item in integer_list:
+        if item % 2 == 0:
+            evens.append(item)
+
+    # return highest number in evens list 
+    return max(evens) 
 
 print(highest_even([10,2,3,4,8,11]))
