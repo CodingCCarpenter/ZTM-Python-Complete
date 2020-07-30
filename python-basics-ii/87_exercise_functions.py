30669ae02c02facb8c25143e308f6299efb5fcdb
CodingCCarpenter <Christineassists@aol.com>
2020-07-29 21:08:45 -0500

    define problem and create scaffolding for solution function

---
+++ python-basics-ii/87_exercise_functions.py
@@ -0,0 +1,11 @@
+""" 
+create a function called highest_even that takes a list
+as an argument, and returns the highest even number 
+ 
+ """
+
+
+def highest_even(integer_list):
+    pass
+
+print(highest_even([10,2,3,4,8,11]))
