b66dc4b132842519b5e62d5b0e01ba4fce88f2f9
CodingCCarpenter <Christineassists@aol.com>
2020-07-29 21:18:24 -0500

    cleaning up unstaged changes

--- python-basics-ii/86_args_and_kwargs.py
+++ python-basics-ii/86_args_and_kwargs.py
@@ -1,6 +1,7 @@
 # *args and **kwargs
 
 def super_func(*args, **kwargs):
+    # initialize total at 0 for kwargs for loop
     total=0
     # will print sum of all args entered:
     args_sum = sum(args)
@@ -18,3 +19,17 @@ print(result)
 # *args allows us to tell it to perform the action on everything given during function call
 # **kwargs allows us to enter key value pairs to be stored in a dictionary.
 
+"""
+!!!!! There is a rule we must follow 
+about the order of our parameters. !!!!!
+
+The order goes:
+    1. parameters
+    2. *args
+    3. default parameters
+    4. **kwargs
+
+def psuedo_code(parameters, *args, default_params='this', **kwargs):
+    print(f"first comes {parameters}, then {*args} followed by {default_params} and finally {kwargs}")
+
+"""
