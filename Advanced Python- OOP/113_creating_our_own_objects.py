a720b1add2a663cfaae1af31e22c2d01d9556dae
CodingCCarpenter <Christineassists@aol.com>
2020-08-05 17:39:54 -0500

    add notes to __init__ and run methods to explain the purpose of each

--- Advanced Python- OOP/113_creating_our_own_objects.py
+++ Advanced Python- OOP/113_creating_our_own_objects.py
@@ -1,8 +1,12 @@
 # Creating our own objects with OOP
 
 class PlayerCharacter:
+    # built in dunder method that acts as constructor - params other than 'self' will be required
     def __init__(self, name):
         self.name = name
 
+    # custom method that we created
     def run(self):
         print('run')
+
+player1 = PlayerCharacter('Cindy')
