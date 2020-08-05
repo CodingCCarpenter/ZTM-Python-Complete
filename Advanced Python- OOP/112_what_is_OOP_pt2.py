178e956fe05cab9f8265266607864283522b54a6
CodingCCarpenter <Christineassists@aol.com>
2020-08-05 17:32:01 -0500

    demonstrate difference between classes and objects in Python

---
+++ Advanced Python- OOP/112_what_is_OOP_pt2.py
@@ -0,0 +1,16 @@
+# OOP
+
+# This is the class, which is like the blueprints for making objects.
+class BigObject: 
+    pass
+
+# Each time we instanciate our class, (call and store it), we create an object
+
+# 1st instance/object
+obj1 = BigObject()
+
+# 2nd instance/object
+obj2 = BigObject()
+
+#3rd instance/object
+obj3 = BigObject()
