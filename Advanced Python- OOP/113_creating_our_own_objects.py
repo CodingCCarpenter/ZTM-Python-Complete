16d44639ae792a58530cf0731ba64970d7125985
CodingCCarpenter <Christineassists@aol.com>
2020-08-05 18:22:44 -0500

    create two instances of PlayerCharacter - player1 and player2

--- Advanced Python- OOP/113_creating_our_own_objects.py
+++ Advanced Python- OOP/113_creating_our_own_objects.py
@@ -2,17 +2,23 @@
 
 class PlayerCharacter:
     # built in dunder method that acts as constructor - params other than 'self' will be required
-    def __init__(self, name):
+    def __init__(self, name, role):
         self.name = name
+        self.role = role
 
     # custom method that we created
-    def run(self):
-        print('run')
+    def battle_cry(self):
+        print('YAAAAAAAAAAARG!!!!')
 
-player1 = PlayerCharacter('Cindy')
+# creating a first player
+player1 = PlayerCharacter('Bacchus', 'Viking ')
 
 """
 self acts a lot like 'this' in JavaScript. It makes 
 the parameters other than 'self' required, and it 
 allows the function to refer to its own data. 
 """
+
+# adding a second player (instance of PlayerCharacter)
+player2 = PlayerCharacter('Krystyne', 'Elf')
+
