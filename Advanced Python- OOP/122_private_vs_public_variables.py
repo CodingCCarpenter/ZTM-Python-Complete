7635f52be7f7cf350b54405ed6dd01cbef94612e
CodingCCarpenter <Christineassists@aol.com>
2020-08-05 21:14:50 -0500

    demonstrate use of _ to indicate to other developers that the variable should not be altered

--- Advanced Python- OOP/122_private_vs_public_variables.py
+++ Advanced Python- OOP/122_private_vs_public_variables.py
@@ -1,20 +1,26 @@
+# can we make a private variable???
+# there are no truly private variables
+# using _ before a variable, it tells us that we shouldn't change it
 class PlayerCharacter():
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        self._name = name
+        self._age = age
 
     def run(self):
         print('run')
 
-    def shout(self):
-        print (f'my name is {self.name} and I am {self.age} years old.')
+    def _shout(self):
+        print (f'my name is {self._name} and I am {self._age} years old.')
         
 player1 = PlayerCharacter('Christy', 32)
 # when we call a specific method, we are seeing abstraction at work
 # python gives us only the method, and not all of the information related to the player
 print(player1.run())
-print(player1.shout())
+print(player1._shout())
 
 # our methods and attributes can be easily overwritten by default! Oh nooooooo!
-player1.speak = 'BOOOOOOO! HISS!'
-print(player1.speak)
+# good thing we used the underscore! This means 'please don't alter this'
+
+player1.shout = 'BOOOOOOO! HISS!'
+print(player1.shout)
+print(player1._shout())
