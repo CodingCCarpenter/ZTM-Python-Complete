37d217c68edb979afa34b0b6fc11eb3872f35a96
CodingCCarpenter <Christineassists@aol.com>
2020-08-05 19:16:46 -0500

    demonstrate mastery of previous lessons

--- Advanced Python- OOP/116_exercise_cats_everywhere.py
+++ Advanced Python- OOP/116_exercise_cats_everywhere.py
@@ -10,11 +10,16 @@ class Cat:
 
 
 # 1 Instantiate the Cat object with 3 cats
-
+patches = Cat('Patches', 3)
+thai_chi = Cat('Thai Chi', 2)
+kermit = Cat('Kermit', 6)
 
 
 # 2 Create a function that finds the oldest cat
+cat_ages = [patches.age, thai_chi.age, kermit.age]
 
-
+def oldest_cat():
+    return age_of_oldest = max(cat_ages)
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f"The oldest cat is {age_of_oldest} years old")
