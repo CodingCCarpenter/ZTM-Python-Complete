1ea539cce61261594892448771b63b81420bc741
CodingCCarpenter <Christineassists@aol.com>
2020-08-05 20:51:12 -0500

    create a method to instantiate a new object of a class

--- Advanced Python- OOP/117_at-classmethod_and_at-staticmethod.py
+++ Advanced Python- OOP/117_at-classmethod_and_at-staticmethod.py
@@ -21,6 +21,21 @@ class PlayerCharacter:
         player1.attack = current_attack + 1
         return current_attack
 
+    @classmethod
+
+    # creating another classmethod that instantiates a new player
+    def add_player(cls, name, role, num1, num2):
+        return cls(name, role, num1 + num2)
+
+    # @staticmethod works the same way as static method, except you can't access the class
+    # use where we don't care about the class state or attributes
+    @staticmethod
+
+    def add (num1, num2):
+        num1 + num2
+
+
+
 # create an instance of PlayerCharacter
 player1 = PlayerCharacter('Bacchus', 'Viking', 53)
 
@@ -40,3 +55,8 @@ player1.add_attack_point(player1.attack)
 # printing current attack points
 print(player1.attack)
 
+# creating a new player using the classmethod add_player
+player2 = PlayerCharacter.add_player('Chloe', 'Elf', 18, 12)
+
+# testing that player2 was created by attempting to access player2's stats
+print(player2.name, player2.role, player2.attack)
