831fffd11faf61665d50bea39b46ee83861ce2a5
CodingCCarpenter <Christineassists@aol.com>
2020-08-07 16:21:11 -0500

    further refine code in polymorphism example to include a return for everymethod on each class

--- Advanced Python- OOP/125_polymorphism.py
+++ Advanced Python- OOP/125_polymorphism.py
@@ -19,8 +19,18 @@ class User():
 class Wizard(User):
     role = 'wizard'
 
-    def _fireball(self, target):
+    """because of polymorphism, we can have a 
+    _basic_attack method defined both here and 
+    in the Archer class so that it behaves  
+    differently based on which class it is called
+    on"""
+    def _basic_attack(self, target):
         target.health = target.health - 3
+        print('Wizard bops you on the head with their staff. You take 3 damage.')
+        return target.health
+    
+    def _fireball(self, target):
+        target.health = target.health - 7
         print('FIREBALL!!! You take 3 damage!')
         return target.health
 
@@ -29,20 +39,33 @@ class Wizard(User):
             self.health += 5
             print('A healing light surrounds you! Heal 5 points!')
             return self.health
-        print('You did not need to heal. You wasted your turn!')
+        print( 'You did not need to heal. You wasted your turn!')
 
 class Archer(User):
     role = 'archer'
 
-    def _crack_shot(self, target):
+    def _basic_attack(self, target):
         target.health = target.health - 3
+        print('Archer punches you. You take 3 damage.')
+        return target.health
+    
+    def _crack_shot(self, target):
+        target.health = target.health - 7
         print('The archer never misses! You take 5 damage!')
+        return target.health
 
     def field_medic(self):
         if self.health < 15:
             self.health += 5
             print('Thank goodness for field medics! Heal 5 points!')
+            return self.health
 
 wizard1 = Wizard('Dumbledore')
 
 archer1 = Archer('Robin')
+
+""" The methods have different returns based 
+on which class the character that calls it 
+is an instance of"""
+archer1._basic_attack(wizard1)
+wizard1._basic_attack(archer1)
