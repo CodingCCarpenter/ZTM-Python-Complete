44bd4ca50b8dbc10ced432167c3fcda7a1283047
CodingCCarpenter <Christineassists@aol.com>
2020-08-12 18:00:41 -0500

    demonstrate use of filter

--- 11.Advanced-python-functional-programming/136-139_map_filter_reduce_zip.py
+++ 11.Advanced-python-functional-programming/136-139_map_filter_reduce_zip.py
@@ -1,8 +1,23 @@
 # MAP()
-def multiply_by_2(li):
-    new_list = []
-    for item in li:
-        new_list.append(item*2)
-    return new_list
 
-print(multiply_by_2([1,2,3]))
+def multiply_by_2(item):
+    return item * 2
+
+print(list(map(multiply_by_2, [1,2,3])))
+
+# my own example
+users = ['Amanda', 'Joe', 'Michael', 'Laura', 'Jeremy', 'Lily']
+
+def greet_user(user):
+    return (f'Welcome to the course, {user}')
+
+print(list(map(greet_user, users)))
+
+# FILTER
+
+my_list = [1,2,3]
+
+def grab_odds(item):
+    return item % 2 != 0
+
+print(list(filter(grab_odds, my_list)))
