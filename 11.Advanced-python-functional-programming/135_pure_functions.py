f3ae36de162d0dea09a9c6d71ed7a5460851c493
CodingCCarpenter <Christineassists@aol.com>
2020-08-12 16:15:44 -0500

    add an example function that demonstrates pure functions

---
+++ 11.Advanced-python-functional-programming/135_pure_functions.py
@@ -0,0 +1,10 @@
+# what is a pure function?
+# a pure function given the same input will always return the same output
+# a pure function should not produce any side effects
+
+def multiply_by2(li):
+    new_list = []   
+    for item in li:
+        new_list.append(item*2)
+    return new_list
+        
