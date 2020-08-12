b7b629b67bd1f2720faa394519d4d1215c621d11
CodingCCarpenter <Christineassists@aol.com>
2020-08-12 18:29:18 -0500

    combine all numbers in lists on test file

--- 11.Advanced-python-functional-programming/140_exercises_map_filter_zip_reduce.py
+++ 11.Advanced-python-functional-programming/140_exercises_map_filter_zip_reduce.py
@@ -17,5 +17,15 @@ print(list(zip(my_strings,sorted(my_numbers))))
 #3 Filter the scores that pass over 50%
 scores = [73, 20, 65, 19, 76, 100, 88]
 
+def check_score(score):
+    return score > 50
+
+print(list(filter(check_score, scores)))
 
 #4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+my_list = list(my_numbers + scores)
+
+def accumulator(acc,item):
+    return acc + item
+
+print(reduce(accumulator, my_list, 0))
